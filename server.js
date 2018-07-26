c4d90c726d6875b4c4aaf75abf16a6aa418726d3
moskalenkoBV <b.v.moskalenko@gmail.com>
2018-07-26 22:43:59 +0300

    Config deployment

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-import path from 'path'
-import express from 'express'
+const path = require('path')
+const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => {
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
-app.listen(process.env.PORT || 3000, () => { console.log('Listening...') })
+app.listen(process.env.PORT || 3000)
 
-export default app
+process.noDeprecation = true
+
+console.log('Listening...')
